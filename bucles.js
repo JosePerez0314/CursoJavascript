e878755e7d593c8e71b21ab1df640e3b5b3efbb2
José Pérez <jotangamers@gmail.com>
2023-11-11 20:24:44 -0400

    primer commit

---
+++ bucles.js
@@ -0,0 +1,14 @@
+// bucles en javascript
+
+/* Los bucles son aquellas funciones de javascript que permiten a 
+repetir un bloque de codigo siempre y cuando se repita una condicion */
+
+/* El bucle while es un bucle conocido principalmente porque es un bucle infinito, osea siempre sigue repitiendose
+infinitamente un ejemplo claro */
+
+/* En esta funcion de javascript como podemos ver se va a ejecutar ya que la condicion es verdadera
+ya que 5 si es mayor que 3 */
+
+while( 5 > 3) {
+    console.log("Hola mundo")
+};
